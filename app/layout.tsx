c723c53236d7683a88da015f07ec0f13e5f6cd4d
Debidutta <debiduttaacharya2002@gmail.com>
2026-07-18 19:33:08 +0530

    feat: implement root layout with SEO metadata, custom fonts, providers, and GTM integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -120,23 +120,23 @@ export default function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
-      {/* Google Tag Manager */}
-      <Script
-        id="gtm-head"
-        strategy="beforeInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-K2S2GK3N');`,
-        }}
-      />
-      {/* End Google Tag Manager */}
       <body
         className={`${inter.variable} ${syne.variable} ${jetbrainsMono.variable} font-body`}
         suppressHydrationWarning
       >
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K2S2GK3N');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
